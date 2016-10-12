919854561903f80d3ef9d387641af1d08ef8f7b2
jinzhaopo <412743165@qq.com>
2016-10-12 10:06:07 +0800

    提交更新

---
+++ cloudlib_ios/SynchronousHttpData.h
@@ -0,0 +1,13 @@
+//
+//  SynchronousHttpData.h
+//  cloudlib_ios
+//
+//  Created by jinzhaopo on 16-10-12.
+//  Copyright (c) 2016年 yundao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SynchronousHttpData : NSObject
+
+@end
---
+++ cloudlib_ios/SynchronousHttpData.m
@@ -0,0 +1,13 @@
+//
+//  SynchronousHttpData.m
+//  cloudlib_ios
+//
+//  Created by jinzhaopo on 16-10-12.
+//  Copyright (c) 2016年 yundao. All rights reserved.
+//
+
+#import "SynchronousHttpData.h"
+
+@implementation SynchronousHttpData
+
+@end
