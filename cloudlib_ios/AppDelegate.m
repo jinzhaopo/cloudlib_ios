8e944f3748512888d6a15f1296687142ff69699d
jinzhaopo <412743165@qq.com>
2016-10-20 10:57:35 +0800

    fvsdfbvds

--- cloudlib_ios/AppDelegate.m
+++ cloudlib_ios/AppDelegate.m
@@ -35,8 +35,6 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    ViewController *viewController=[[ViewController alloc] init];
-    self.window.rootViewController=viewController;
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
